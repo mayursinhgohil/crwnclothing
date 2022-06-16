4cb2921e88f7b141552877213e960595e14bfef2
mayursinhgohil <mayur.gohil@changejar.in>
2022-06-16 17:05:54 +0530

    added redirect

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
+import './directory-item.style.scss'
+
+const DirectoryItem = ({ category }) => {
+
+    const { title, imageUrl, route } = category;
+
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
+
+    return (
+        <div className="directory-item-container" onClick={onNavigateHandler}>
+            {/* <img src=""/> */}
+            <div className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }} />
+            <div className="body" >
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default DirectoryItem
