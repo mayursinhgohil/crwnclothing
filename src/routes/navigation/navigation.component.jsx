fefb024396e11616b726c4e8722aa9c3ad3fe3f4
mayursinhgohil <mayur.gohil@changejar.in>
2022-06-30 13:19:39 +0530

    added redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context'
@@ -7,12 +8,15 @@ import { CartContext } from '../../contexts/cart.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+import { selectCurrentUser } from '../../store/user/user.selector'
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
 
-    const { currentUser } = useContext(UserContext)
+    const currentUser = useSelector(selectCurrentUser);
+
+    // const { currentUser  } = useContext(UserContext)
     const { isCartOpen } = useContext(CartContext)
 
     const signOutHandler = async () => {
